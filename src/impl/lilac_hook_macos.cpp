1ad9086193a6f8f28102e31fc92baf065974bb8e
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2021-09-28 08:05:51 +0300

    i think it works now

--- src/impl/lilac_hook_macos.cpp
+++ src/impl/lilac_hook_macos.cpp
@@ -1,13 +1,11 @@
 #include "lilac_hook_macos.hpp"
 #include "lilac_hook_impl.hpp"
 
-#include <dlfcn.h>          // dladdr()
 #include <mach/mach_vm.h>       /* mach_vm_*            */
 #include <mach/mach_init.h>     /* mach_task_self()     */
 
 #include <signal.h>             /* sigaction            */
-#include <sys/ucontext.h>
-#include <iostream>
+#include <sys/ucontext.h>       /* ucontext_t           */
 
 using namespace lilac;
 using namespace impl;
@@ -31,15 +29,15 @@ namespace {
 
 void MacOSX::write_memory(void* to, const void* from, size_t size) {
     kern_return_t ret;
-
-    ret = mach_vm_protect(mach_task_self(), (mach_vm_address_t)to, size, FALSE, VM_PROT_WRITE | VM_PROT_READ | VM_PROT_EXECUTE);
+    
+    ret = mach_vm_protect(mach_task_self(), (mach_vm_address_t)to, size, FALSE, VM_PROT_COPY | VM_PROT_EXECUTE | VM_PROT_READ | VM_PROT_WRITE);
     if (ret != KERN_SUCCESS) return;
 
     ret = mach_vm_write(mach_task_self(), (mach_vm_address_t)to, (vm_offset_t)from, (mach_msg_type_number_t)size);
-    if (ret != KERN_SUCCESS) return;
 }
 
 bool MacOSX::initialize() {
+    volatile int a = init; /* epic bugfixing */
     struct sigaction action;
     memset(&action, '\0', sizeof(action));
     action.sa_sigaction = &handler;
